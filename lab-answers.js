b9f1de9be4a7c224fdb838e7770a07e208ea84ca
Biscolad <biscolad@gmail.com>
2023-10-22 00:32:02 -0400

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -7,3 +7,22 @@ for (let i=0; i<=20; i++) {
 for (let i=2; i<=200; i+=2) {
     console.log(i)
 }
+
+//Fizz Buzz
+const numbLog = [];
+for (let i=1; i<=100; i++) 
+if (i%3===0 && i%5===0) {
+    numbLog.push("Fizzbuzz")
+}
+else if (i%3 === 0) {
+    numbLog.push("Fizz") 
+    }
+else if (i%5 === 0) {
+    numbLog.push("Buzz")
+    }
+else {
+    numbLog.push(i)
+}
+console.log(numbLog)
+
+
